39a1602b860fc3ceefa18809ce4b72c128ef19c2
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2023-10-15 09:48:26 +0500

    fix serilog config error, note QoL bug in Serilog

--- API/Program.cs
+++ API/Program.cs
@@ -6,13 +6,16 @@ Log.Logger = new LoggerConfiguration()
         new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", true) // props: path, reloadOnChange
-            /* reload if below configuration changes (,true) */
+                                                   // reload if below configuration changes (,true) 
             .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
             .Build()
     )
     .CreateLogger();
 
-/* 
+/* OT: USE ONLY FOR TESTING IF ANY ISSUES READING FROM APPSETTINGS, BELOW CONFIG IS
+ * AN EXTENSION METHOD REPRESENTATION OF THE BASE STRUCTURE LAID  IN CONFIGURATION FILE, JUST
+ * COPY THE CODE BELOW AND LEAVE COMMENTED AREA AS IS FOR TESTING.
+
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.File(
@@ -20,7 +23,7 @@ Log.Logger = new LoggerConfiguration()
         rollingInterval: RollingInterval.Day,
         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [LC:{Level:u3}/UC:{Level:w3}] {Message:lj} {NewLine} {Properties:j}{NewLine}{Exception}"
     )
-    .Enrich.With(new ThreadIDEnricher())
+    .Enrich.With(new ThreadIDEnricher()) // order does-not matter, above and below sinks will have ThreadID property
         .WriteTo.Console(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error)
         .Enrich.WithProperty("Version", "6.9.6")
             .WriteTo.Seq("http://192.168.100.115:5341")
